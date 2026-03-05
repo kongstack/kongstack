851b20a1e3072e83505e2dfee4e1bee9dca786fd
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2026-03-05 16:23:07 -0600

    feat: updated playground

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -3,10 +3,9 @@ import { Typography, TypoProvider } from '@kongstack/typo-react'
 function App() {
   return (
     <TypoProvider>
-      <Typography as="h1">Hello KongStack!</Typography>
-      <Typography as="h2">Hello KongStack!</Typography>
-      <Typography as="h3">Hello KongStack!</Typography>
-      <Typography as="p">This is typo-react working!</Typography>
+      <Typography variant="h1" as="h1">Heading 1</Typography>
+    <Typography variant="body" as="p">Body text</Typography>
+    <Typography variant="overline" as="span">Overline</Typography>
     </TypoProvider>
   )
 }
